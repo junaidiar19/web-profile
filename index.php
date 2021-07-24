7ed120f89bb9b0d1bfc408a3a49713dbc451454f
Junaidi Abdul Rahman <junaidiar.fcb@gmail.com>
2021-07-24 20:27:35 +0800

    prod and dev database.php

--- index.php
+++ index.php
@@ -53,7 +53,20 @@
  *
  * NOTE: If you change these, also change the error_reporting() code below
  */
-	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+
+switch ($_SERVER['HTTP_HOST']) {
+	case 'localhost':
+		$env = 'development';
+		break;
+	case 'www.zulaika.polhas.xyz':
+		$env = 'production';
+		break;
+	default:
+		$env = 'development';
+		break;
+}
+
+	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : $env);
 
 /*
  *---------------------------------------------------------------
